8902fa99771abc2f092072ff273b0db6f9771fd3
Anthony Regulado <ajregulado@gmail.com>
2023-11-08 15:46:51 +0800

    Rename project labels

--- containers/Projects.tsx
+++ containers/Projects.tsx
@@ -15,7 +15,7 @@ const Projects = () => {
               </div>
             </div>
             <div className="pl-4">
-              <h4 className="display-3 text-info">Projects</h4>
+              <h4 className="display-3 text-info">Project Involvements</h4>
             </div>
           </div>
           <Row className="row-grid align-items-center">
